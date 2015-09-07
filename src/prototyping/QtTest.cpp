05b003007ffbe19464f476e14ca47e7d745c9731
David Saxon <davidesaxon@gmail.com>
2015-09-07 23:32:16 +1200

    testing qt

--- src/prototyping/QtTest.cpp
+++ src/prototyping/QtTest.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
 #include <QtWidgets/QApplication>
 
-#include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QPushButton>
+#include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
 
 int main( int argc, char* argv[] )
 {
     QApplication app( argc, argv );
 
-    QLabel* label = new QLabel( "Hello World" );
-    label->show();
+    QWidget* window = new QWidget();
+
+    QVBoxLayout* layout = new QVBoxLayout();
+    window->setLayout( layout );
+
+    QLabel* label = new QLabel( "SIGMA" );
+    layout->addWidget( label );
+
+    QPushButton* button = new QPushButton( "START" );
+    layout->addWidget( button );
+
+    window->show();
 
     QApplication::exec();
 
