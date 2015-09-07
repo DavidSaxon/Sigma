06a4294d21e2388fd7b3e0f6d66ef145d5a6fc8f
David Saxon <davidesaxon@gmail.com>
2015-09-07 23:22:57 +1200

    renamed windows build directory

--- src/prototyping/QtTest.cpp
+++ src/prototyping/QtTest.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <QtWidgets/QApplication>
+
+#include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QLabel>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QWidget>
 
 int main( int argc, char* argv[] )
 {
