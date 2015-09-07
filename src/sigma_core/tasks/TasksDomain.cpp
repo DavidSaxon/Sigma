1b391abb114583bb753cfc4205ce45e9a0995b46
David Saxon <davidesaxon@gmail.com>
2015-09-08 01:30:04 +1200

    Started work on tasks

---
+++ src/sigma_core/tasks/TasksDomain.cpp
@@ -0,0 +1,9 @@
+#include "sigma_core/tasks/Domain.hpp"
+
+namespace sigma_core
+{
+namespace tasks
+{
+
+} // namespace tasks
+} // namespace sigma_core
---
+++ src/sigma_core/tasks/TasksDomain.hpp
@@ -0,0 +1,13 @@
+#ifndef SIGMA_CORE_TASKS_DOMAIN_HPP_
+#   define SIGMA_CORE_TASKS_DOMAIN_HPP_
+
+namespace sigma_core
+{
+// TODO: document namespace
+namespace tasks
+{
+
+} // namespace tasks
+} // namespace sigma_core
+
+#endif
